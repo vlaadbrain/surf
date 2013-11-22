71e284a60832a378843d57ff18cc8c7c968d5baf
Carlos J. Torres <vlaadbrain@gmail.com>
2013-11-22 14:18:51 -0500

    zoom to 95%

--- config.h
+++ config.h
@@ -15,7 +15,7 @@ static Bool zoomto96dpi     = TRUE;  /* Zoom pages to always emulate 96dpi */
 static Bool runinfullscreen = FALSE; /* Run in fullscreen mode by default */
 
 static guint defaultfontsize = 12;   /* Default font size */
-static gfloat zoomlevel = -1.0;       /* Default zoom level */
+static gfloat zoomlevel = 0.95;       /* Default zoom level */
 
 /* Webkit default features */
 static Bool enablescrollbars = TRUE;
