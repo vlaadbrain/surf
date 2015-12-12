4a89ef872a5bd38598bebfb4c6d9c85d8732f9f7
Carlos J. Torres <vlaadbrain@gmail.com>
2015-12-11 23:21:06 -0500

    update config.h

--- config.h
+++ config.h
@@ -1,11 +1,12 @@
 /* modifier 0 means no modifier */
 static char *useragent      = "Mozilla/5.0 (X11; U; Unix; en-US) "
-	"AppleWebKit/537.15 (KHTML, like Gecko) Chrome/24.0.1295.0 "
-	"Safari/537.15 Surf/"VERSION;
-static char *stylefile      = "~/.surf/style.css";
+	                          "AppleWebKit/537.15 (KHTML, like Gecko) "
+							  "Chrome/24.0.1295.0 Safari/537.15 Surf/"VERSION;
 static char *scriptfile     = "~/.surf/script.js";
+static char *styledir       = "~/.surf/styles/";
+static char *cachefolder    = "~/.surf/cache/";
 
-static Bool kioskmode	    = FALSE; /* Ignore shortcuts */
+static Bool kioskmode       = FALSE; /* Ignore shortcuts */
 static Bool showindicators  = TRUE;  /* Show indicators in window title */
 static Bool zoomto96dpi     = TRUE;  /* Zoom pages to always emulate 96dpi */
 static Bool runinfullscreen = FALSE; /* Run in fullscreen mode by default */
@@ -18,41 +19,67 @@ static char *cookiefile     = "~/.surf/cookies.txt";
 static char *cookiepolicies = "@aA"; /* A: accept all; a: accept nothing,
                                         @: accept no third party */
 static char *cafile         = "/etc/ssl/certs/ca-certificates.crt";
-static char *strictssl      = FALSE; /* Refuse untrusted SSL connections */
+static Bool strictssl       = FALSE; /* Refuse untrusted SSL connections */
 static time_t sessiontime   = 3600;
 
 static char *downloadsdir   = "~/Downloads";
 
 /* Webkit default features */
-static Bool enablescrollbars = TRUE;
+static Bool enablescrollbars      = TRUE;
 static Bool enablespatialbrowsing = TRUE;
-static Bool enableplugins = TRUE;
-static Bool enablescripts = TRUE;
-static Bool enableinspector = TRUE;
-static Bool loadimages = TRUE;
-static Bool hidebackground  = FALSE;
-static Bool allowgeolocation = TRUE;
+static Bool enablediskcache       = TRUE;
+static int diskcachebytes         = 5 * 1024 * 1024;
+static Bool enableplugins         = FALSE;
+static Bool enablescripts         = FALSE;
+static Bool enableinspector       = FALSE;
+static Bool enablestyle           = FALSE;
+static Bool loadimages            = TRUE;
+static Bool hidebackground        = FALSE;
+static Bool allowgeolocation      = FALSE;
 
 #define SETPROP(p, q) { \
 	.v = (char *[]){ "/bin/sh", "-c", \
-		"prop=\"`xprop -id $2 $0 | cut -d '\"' -f 2 | xargs -0 printf %b | dmenu -p 'surf> '`\" &&" \
-		"xprop -id $2 -f $1 8s -set $1 \"$prop\"", \
-		p, q, winid, NULL \
+	     "prop=\"`xprop -id $2 $0 " \
+	     "| sed \"s/^$0(STRING) = \\(\\\\\"\\?\\)\\(.*\\)\\1$/\\2/\" " \
+	     "| xargs -0 printf %b | dmenu -p 'surf >'`\" &&" \
+	     "xprop -id $2 -f $1 8s -set $1 \"$prop\"", \
+	     p, q, winid, NULL \
 	} \
 }
 
 /* DOWNLOAD(URI, referer) */
 #define DOWNLOAD(d, r) { \
 	.v = (char *[]){ "/bin/sh", "-c", \
-		"st -e /bin/sh -c \"cd $4; curl -L -J -O --user-agent '$1'" \
-		" --referer '$2' -b $3 -c $3 '$0';" \
-		" sleep 5;\"", \
-		d, useragent, r, cookiefile, downloadsdir, NULL \
+	     "st -e /bin/sh -c \"cd $4; curl -L -J -O --user-agent '$1'" \
+	     " --referer '$2' -b $3 -c $3 '$0';" \
+	     " sleep 5;\"", \
+	     d, useragent, r, cookiefile, downloadsdir, NULL \
 	} \
 }
 
 #define OPENBOOKMARK(p) { .v = (char *[]){ "/bin/sh", "-c", "openbm.sh $0 $1", winid, p, NULL } }
 #define SAVEBOOKMARK { .v = (char *[]){ "/bin/sh", "-c", "savebm.sh $0", winid, NULL } }
+
+/* PLUMB(URI) */
+/* This called when some URI which does not begin with "about:",
+ * "http://" or "https://" should be opened.
+ */
+#define PLUMB(u) {\
+	.v = (char *[]){ "/bin/sh", "-c", \
+	     "xdg-open \"$0\"", u, NULL \
+	} \
+}
+
+/* styles */
+/*
+ * The iteration will stop at the first match, beginning at the beginning of
+ * the list.
+ */
+static SiteStyle styles[] = {
+	/* regexp               file in $styledir */
+	{ ".*",                 "default.css" },
+};
+
 #define MODKEY GDK_CONTROL_MASK
 
 /* hotkeys */
@@ -107,6 +134,16 @@ static Key keys[] = {
     { MODKEY|GDK_SHIFT_MASK,GDK_bar,    togglescrollbars,{ 0 } },
     { MODKEY|GDK_SHIFT_MASK,GDK_g,      togglegeolocation, { 0 } },
 	{ MODKEY|GDK_SHIFT_MASK,GDK_a,      togglecookiepolicy, { 0 } },
+};
+/* button definitions */
+/* click can be ClkDoc, ClkLink, ClkImg, ClkMedia, ClkSel, ClkEdit, ClkAny */
+static Button buttons[] = {
+	/* click        event mask  button  function        argument */
+	{ ClkLink,      0,          2,      linkopenembed,  { 0 } },
+	{ ClkLink,      MODKEY,     2,      linkopen,       { 0 } },
+	{ ClkLink,      MODKEY,     1,      linkopen,       { 0 } },
+	{ ClkAny,       0,          8,      navigate,       { .i = -1 } },
+	{ ClkAny,       0,          9,      navigate,       { .i = +1 } },
 };
 
 static SearchEngine searchengines[] = {
